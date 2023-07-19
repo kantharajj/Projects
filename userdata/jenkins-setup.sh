d3c82cd311f78d8206235cf7043950ee9c9973e5
kantharajj <103110868+kantharajj@users.noreply.github.com>
2023-07-19 14:22:59 +0530

    Update jenkins-setup.sh

--- userdata/jenkins-setup.sh
+++ userdata/jenkins-setup.sh
@@ -1,13 +1,11 @@
+
+
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
-sudo apt-get install jenkins
-sudo apt update
-sudo apt-get install openjdk-17-jdk
-java -version
-openjdk version "17.0.7" 2023-04-18
-OpenJDK Runtime Environment (build 17.0.7+7-Debian-1deb11u1)
-OpenJDK 64-Bit Server VM (build 17.0.7+7-Debian-1deb11u1, mixed mode, sharing)
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+sudo apt update -y
+sudo apt install openjdk-11* -y 
