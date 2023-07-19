4cd7c92f71cbda823079da017dfb493b190ce615
kantharajj <103110868+kantharajj@users.noreply.github.com>
2023-07-19 14:24:12 +0530

    Update jenkins-setup.sh

--- userdata/jenkins-setup.sh
+++ userdata/jenkins-setup.sh
@@ -1,5 +1,3 @@
-
-
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
